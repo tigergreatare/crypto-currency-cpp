96b05f385931d16b319427f1ac20a2c4c87ec455
Alaa Sbai Abbassi <alaamorroco@gmail.com>
2025-01-10 11:34:47 -0500

    Initial commit

---
+++ ConsoleApplication1/main.cpp
@@ -0,0 +1,15 @@
+// main.cpp
+#include "MerkelMain.h"
+#include "OrderBookEntry.h"
+#include "CSVReader.h"
+#include "Wallet.h"
+int main() {
+    MerkelMain app;
+    app.init();
+   /* Wallet wallet;
+    wallet.insertCurrency("BTC", 10);
+    wallet.insertCurrency("USDT", 100000);
+    wallet.removeCurrency("USDT", 1000);
+    cout << wallet.toString() << endl;*/
+    /*CSVReader::readCSV("C:\\Users\\alaad\\Desktop\\bullshit\\to be organized 2.0\\20200317.csv");*/
+}
